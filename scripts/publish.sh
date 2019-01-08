c022e56638d7ce797d312ed82a68c7ed4c4dac5c
Conor Sheehan <conor.sheehan.dev@gmail.com>
2019-01-08 11:07:57 +0000

    fix patch, changes applied in patch and source, remove changes in source remake orig rename publish to sign changes make script that actually publishes ppa space separated dist didn't work, use signle dist

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $SCRIPTPATH/../fits-1.4.0
-sudo debuild -S -sa
+cd $SCRIPTPATH/..
+dput -f ppa:conorsheehan1/fits-ppa fits_1.4.0-1_source.changes
